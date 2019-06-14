43bc414608e299460f32e794e66505ea02a7c977
Dani Palou <dani@moodle.com>
2019-06-14 09:01:12 +0200

    Translate to spanish and catalan

---
+++ lang/ca/atto_applink.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'atto_applink', language 'ca'.
+ *
+ * @package    atto_applink
+ * @copyright  2019 Dani Palou <dani@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addusername'] = 'Afegir nom d\'usuari';
+$string['browserepositories'] = 'Navega pels repositoris...';
+$string['createlink'] = 'Insereix un enllaç web';
+$string['enterurl'] = 'Introdueix un URL';
+$string['openinapp'] = 'Obrir a l\'app';
+$string['openinnewwindow'] = 'Obre en una finestra nova';
+$string['pluginname'] = 'Filtre d\'Enllac a l\'app de Moodle per Atto';
+$string['privacy:metadata'] = 'El connector atto_applink no emmagatzema informació personal.';
+$string['urlscheme'] = 'Esquema URL';
+$string['urlscheme_help'] = 'Si vols aplicar un esquema URL específic a aquest enllaç, introdueix-lo en aquest camp. Si el camp és buit, s\'aplicarà l\'esquema per defecte.';
+$string['username'] = 'Nom d\'usuari';
+$string['username_help'] = 'Si vols que l\'app s\'obri amb un usuari específic, introdueix el seu nom d\'usuari en aquest camp. Si "Afegir nom d\'usuari" està habilitat i aquest camp és buit, s\'utilitzarà el nom d\'usuari de l\'usuari que estigui veient l\'enllaç.';
---
+++ lang/es/atto_applink.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'atto_applink', language 'en'.
+ *
+ * @package    atto_applink
+ * @copyright  2019 Dani Palou <dani@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addusername'] = 'Añadir nombre de usuario';
+$string['browserepositories'] = 'Examinar repositorios...';
+$string['createlink'] = 'Crear enlace';
+$string['enterurl'] = 'Introducir URL';
+$string['openinapp'] = 'Abrir en la app';
+$string['openinnewwindow'] = 'Abrir en una nueva ventana';
+$string['pluginname'] = 'Filtro de Enlace a la app de Moodle para Atto';
+$string['privacy:metadata'] = 'La extensión atto_applink no almacena información personal.';
+$string['urlscheme'] = 'Esquema URL';
+$string['urlscheme_help'] = 'Si quieres aplicar un esquema URL específico a este enlace, introdúcelo en este campo. Si el campo está vacío, se aplicará el esquema por defecto.';
+$string['username'] = 'Nombre de usuario';
+$string['username_help'] = 'Si quieres que la app se abra con un usuario específico, introduce su nombre de usuario en este campo. Si "Añadir nombre de usuario" está habilitado i este campo está vacío, se utilizará el nombre de usuario del usuario que esté viendo el enlace.';
